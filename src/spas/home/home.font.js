1f91402032ecdeff1a772fdf9334a9c9f22f1e51
Ammarkarachiwala <ammar.karachiwala@gmail.com>
2018-07-19 18:12:04 -0500

    changed images to light bulb

--- src/spas/home/home.font.js
+++ src/spas/home/home.font.js
@@ -3,7 +3,7 @@ const BUNDLE_HELPER = require('../../utils')
 module.exports = {
     files: [
       './icons/beer.svg',
-      './icons/thumbs-up.svg'
+      './icons/light-bulb.svg'
     ],
     fileName: BUNDLE_HELPER.path(__dirname) + '/fonts.[ext]'
   }
--- src/spas/home/toolbar/index.js
+++ src/spas/home/toolbar/index.js
@@ -8,12 +8,12 @@ export const Toolbar = ({
 }) => {
     return (<div className="toolbar">
             <div>
-                {Array(maxPoints - points).fill().map(i =>  <span style={{ opacity: 0.1 }} className="icon icon-beer"/>)}
                 {Array(points).fill().map(i => <span className="icon icon-beer"/>)}
+                {Array(maxPoints - points).fill().map(i =>  <span style={{ opacity: 0.1 }} className="icon icon-beer"/>)}
             </div>
             <h4>Kitty Clicker</h4>
             <div>
-                {Array(misses).fill().map(i => <span className="icon icon-thumbs-up" />)}
+                {Array(misses).fill().map(i => <span className="icon icon-light-bulb" />)}
             </div>
         </div>);
 }
