9589024a0cb29bcb4ec735ed10776d5b98214dba
Evan Heisler <evanheisler@gmail.com>
2018-10-23 19:39:33 -0500

    init: prettier; organize files

---
+++ src/spas/home/assets/home.font.js
@@ -0,0 +1,6 @@
+const BUNDLE_HELPER = require('../../../utils');
+
+module.exports = {
+  files: ['./icons/beer.svg'],
+  fileName: BUNDLE_HELPER.path(__dirname) + '/fonts.[ext]'
+};
--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import './styles.scss'
-import GithubKitty from './github.svg'
-import './home.font'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+import GithubKitty from './assets/github.svg';
+import './assets/home.font';
 
 class HomeSPA extends Component {
-    render(){
-        return (
-            <div>
-                <span className="icon icon-beer"/>
-                <GithubKitty />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <span className="icon icon-beer" />
+        <GithubKitty />
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render(<HomeSPA />, document.getElementById('react-spa'))
+ReactDOM.render(<HomeSPA />, document.getElementById('react-spa'));
