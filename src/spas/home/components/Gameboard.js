9fde5758ecca9ed5dc31badc6adf1d3103a584e9
JustinSHong <jhong2009@gmail.com>
2018-11-14 00:26:55 -0800

    clicking kitty icon triggers keepScore() to increment score

--- src/spas/home/components/Gameboard.js
+++ src/spas/home/components/Gameboard.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import GithubKitty from "../github.svg";
 
 class Gameboard extends Component {
     render() {
-        return <div className="board" />;
+        return (
+            <div className="board">
+                <GithubKitty fill="red" onClick={this.props.keepScore} />
+            </div>
+        );
     }
 }
 
