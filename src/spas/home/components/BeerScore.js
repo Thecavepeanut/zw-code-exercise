d3fec060cfce191945b24c63aec4793d9bd56985
JustinSHong <jhong2009@gmail.com>
2018-11-14 00:13:51 -0800

    set up BeerScore component above GameBoard

---
+++ src/spas/home/components/BeerScore.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const BeerScore = props => {
+    return (
+        <div>
+            <h3>Beer score here</h3>
+        </div>
+    );
+};
+
+export default BeerScore;
--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -7,12 +7,14 @@ import GithubKitty from "./github.svg";
 import "./home.font";
 // components
 import GameBoard from "./components/GameBoard";
+import BeerScore from "./components/BeerScore";
 
 class HomeSPA extends Component {
     render() {
         return (
             <div className="container">
                 <h1>Github Kitty Game</h1>
+                <BeerScore />
                 <GameBoard />
             </div>
         );
