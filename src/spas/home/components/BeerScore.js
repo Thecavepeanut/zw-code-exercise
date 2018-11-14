8e85e130569adc99fec5bc2db5c4fc16562fd349
JustinSHong <jhong2009@gmail.com>
2018-11-14 00:51:41 -0800

    set up beer icon to render with each point scored

--- src/spas/home/components/BeerScore.js
+++ src/spas/home/components/BeerScore.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
-const BeerScore = props => {
-    return (
-        <div>
-            <h3>Beer score here</h3>
-        </div>
-    );
+const BeerScore = ({ score }) => {
+    const beerPoints = [];
+    if (score !== 10) {
+        for (let i = 0; i < score; i++) {
+            beerPoints.push(
+                <span
+                    className="icon icon-beer"
+                    style={{
+                        fontSize: "40px"
+                    }}
+                />
+            );
+        }
+        return <div>{beerPoints}</div>;
+    } else if (score === 10) {
+        return (
+            <div>
+                <h3>You Win!</h3>
+            </div>
+        );
+    }
 };
 
 export default BeerScore;
