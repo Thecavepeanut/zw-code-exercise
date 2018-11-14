515037a40ef4c3973386776da2e8f6085007d3ad
JustinSHong <jhong2009@gmail.com>
2018-11-14 00:08:13 -0800

    centered game board and made it responsive

--- src/spas/home/components/Gameboard.js
+++ src/spas/home/components/Gameboard.js
@@ -2,11 +2,7 @@ import React, { Component } from "react";
 
 class Gameboard extends Component {
     render() {
-        return (
-            <div>
-                <h1>Game board here</h1>
-            </div>
-        );
+        return <div className="board" />;
     }
 }
 
