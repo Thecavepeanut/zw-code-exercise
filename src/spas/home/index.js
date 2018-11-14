8c5cd19868d6d686f6751905e4ab7dcdb00f69cc
JustinSHong <jhong2009@gmail.com>
2018-11-14 00:26:07 -0800

    set state to track score and passed handleIncreaseScore as props

--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -2,20 +2,38 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 // styles
 import "./styles.scss";
-// iconts
-import GithubKitty from "./github.svg";
+// icons
 import "./home.font";
 // components
 import GameBoard from "./components/GameBoard";
 import BeerScore from "./components/BeerScore";
 
 class HomeSPA extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 0
+        };
+
+        this.handleIncreaseScore = this.handleIncreaseScore.bind(this);
+    }
+
+    // increment score by 1 when a player clicks kitty
+    handleIncreaseScore() {
+        this.setState(function(state) {
+            return {
+                score: state.score + 1
+            };
+        });
+    }
+
     render() {
+        console.log(`Score ${this.state.score}`);
         return (
             <div className="container">
                 <h1>Github Kitty Game</h1>
                 <BeerScore />
-                <GameBoard />
+                <GameBoard keepScore={this.handleIncreaseScore} />
             </div>
         );
     }
