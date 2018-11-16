82e3a1d8fd468116468c07faaf01b24d633d9312
javaPhil <philip.whitman01@gmail.com>
2018-11-16 11:22:49 -0800

    make prompt somewhat customizable

--- src/spas/home/components/GamePrompt.js
+++ src/spas/home/components/GamePrompt.js
@@ -3,6 +3,7 @@ import Button from './Button'
 
 const GamePrompt = props => (
   <div className='game-prompt-container'>
+    {props.promptTitle && <h1 className='game-prompt-title'>{props.promptTitle}</h1>}
     <div className='game-prompt-text'>
       {props.promptText}
     </div>
