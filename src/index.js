7984fb1857ff9bafcaefa686ef8e48c5cf7128d3
Khalid Mohamud <kmohamud1@outlook.com>
2018-11-16 11:55:24 -0800

    small change to source index

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 console.log('hello worlds!')
 
-ReactDOM.render(<h1>Hello Worlds!</h1>, document.getElementById('react-spa'))
+ReactDOM.render(<div>Hellow World</div>, document.getElementById('react-spa'))
