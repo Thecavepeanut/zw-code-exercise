f6b9a98c338d7368117927e2872c8fffa48c4d7a
Khalid Mohamud <kmohamud1@outlook.com>
2018-11-16 11:52:48 -0800

    add custom router

--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -1,17 +1,52 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './styles.scss'
-import GithubKitty from './github.svg'
 import './home.font'
 
+import GameScreen from './components/GameScreen/index.js'
+import HomeScreen from './components/HomeScreen'
+import WinnerScreen from './components/WinnerScreen'
+import LoserScreen from "./components/LoserScreen";
+import AvatarScreen from './components/AvatarScreen'
+import RulesScreen from './components/RulesScreen'
+
+
 class HomeSPA extends Component {
-    render(){
-        return (
-            <div>
-                <span className="icon icon-beer"/>
-                <GithubKitty />
-            </div>
-        )
+    constructor(props) {
+        super(props)
+        this.state = {
+            screen: null,
+            playerName: '',
+        }
+
+        this.switchScreens = this.switchScreens.bind(this)
+    }
+    
+    switchScreens (screen, data = {}) {
+        this.setState({ screen})
+        this.setState(data)
+    }
+
+
+    render() {
+        const {screen} = this.state
+
+        switch(screen) {
+            case 'Game':
+                return (<GameScreen {...this.state} switchScreens={this.switchScreens} />);
+            case 'Avatar':
+                return (<AvatarScreen switchScreens={this.switchScreens}/>);
+            case 'Winner':
+                return (<WinnerScreen {...this.state} switchScreens={this.switchScreens}/>);
+            case 'Looser':
+                return (<LoserScreen  {...this.state} switchScreens={this.switchScreens}/>);
+            case 'Rules':
+                return (<RulesScreen switchScreens={this.switchScreens}/>);
+            case 'Home':
+            default:
+                return (<HomeScreen switchScreens={this.switchScreens}/>)
+        }
+    
     }
 }
 
