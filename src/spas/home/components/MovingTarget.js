c3c6374a48f198310abb3672c191f38ff0778ffc
javaPhil <philip.whitman01@gmail.com>
2018-11-16 11:33:38 -0800

    add styles and logic to MovingTarget and Scorecard

--- src/spas/home/components/MovingTarget.js
+++ src/spas/home/components/MovingTarget.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
-import GithubKitty from './github.svg'
+import GithubKitty from '../icons/github.svg'
 
-
-const MovingTarget = props => (
-  <div>
-    <div>Moving Target</div>
-    <GithubKitty />
-  </div>
-)
+class MovingTarget extends React.Component{
+  render(){
+    return (
+      <div className="moving-target-container">
+        <GithubKitty className="github-kitty-icon" onClick={this.props.onClick}/>
+      </div>
+    )
+  }
+}
 
 export default MovingTarget
--- src/spas/home/components/Scorecard.js
+++ src/spas/home/components/Scorecard.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import BeerIcon from '../icons/beer.svg'
 
-const Scorecard = props => (
-  <div>Scorecard</div>
-)
+const Scorecard = props => {
+
+  return <div>
+    {[...Array(props.playerScore).keys()].map((i) => {
+      return <BeerIcon key={i} className='beer-icon'/>
+    })}
+  </div>
+}
 
 export default Scorecard
