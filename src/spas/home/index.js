437b9414e79624e1e75b2dc6101ebbdbeb43f90b
Seth Bailey <seth.h.bailey@gmail.com>
2019-01-04 13:46:57 -0800

    setup with background img

--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -1,19 +1,20 @@
-import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
-import './styles.scss'
-import GithubKitty from './github.svg'
-import './home.font'
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+import GithubKitty from "./github.svg";
+import "./home.font";
 
 class HomeSPA extends Component {
-    render(){
-        return (
-            <div>
-                <span className="icon icon-beer"/>
-                <GithubKitty />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <span className="icon icon-beer" />
+        <div className="cat-box">
+          <GithubKitty className="cat" />
+        </div>
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render(<HomeSPA />, document.getElementById('react-spa'))
+ReactDOM.render(<HomeSPA />, document.getElementById("react-spa"));
