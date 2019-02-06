52e3f081328a37b5d59bc8a7e1a7d61c29120b25
josectello <josectello@gmail.com>
2019-02-05 21:50:38 -0800

    Finish exercise.

--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -1,19 +1,84 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './styles.scss'
 import GithubKitty from './github.svg'
 import './home.font'
 
 class HomeSPA extends Component {
-    render(){
-        return (
-            <div>
-                <span className="icon icon-beer"/>
-                <GithubKitty />
-            </div>
-        )
+  state = {
+    score: 0,
+    gameActive: false,
+    iconPosition: {}
+  }
+
+  componentWillUpdate(nextProps, { gameActive, score }) {
+    // Tapping into subsequent re-renders following initial mount to trigger interval
+    // Calls only when new game is initialized
+    if (this.state.gameActive !== gameActive && score === 0) {
+      this.triggerInterval();
     }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  handleSVGInteraction = () => {
+    const { score, gameActive } = this.state;
+    if (gameActive) {
+      // Avoid mutating state by using +=
+      let nextState = { score: score + 1 };
+      if (score === 9) {
+        nextState = { ...nextState, gameActive: false };
+        clearInterval(this.state.intervalId);
+      }
+      this.setState({ ...nextState }, this.buildGithubKittyPositioning);
+    }
+  };
+
+  triggerInterval = () => {
+    const intervalId = setInterval(this.buildGithubKittyPositioning, 2000);
+    this.setState({ intervalId });
+  }
+
+  initializeGameHandler = () => this.setState({ score: 0, gameActive: true });
+
+  renderGameMessage = () => {
+    const { score } = this.state;
+    let title = score === 0 ? 'Ready to play?' : 'Congrats! You won!';
+    let buttonText = score === 0 ? 'START' : 'RESTART';
+    return (
+      <section className="game-message">
+        <p>{title}</p>
+        <button onClick={this.initializeGameHandler}>{buttonText}</button>
+      </section>
+    )
+  };
+
+  buildGithubKittyPositioning = () => {
+    const randomValue = () => Math.floor(Math.random() * 100);
+    let top = `${randomValue()}%`;
+    let left = `${randomValue()}%`;
+    this.setState({ iconPosition: { top, left } });
+  };
+
+  render() {
+    const { score, iconPosition, gameActive } = this.state;
+    return (
+      <div className="home-spa">
+        <section className="scoreboard">
+          <p className="instructions">Click on the moving GitHub Kitty icon to score points. 10 points wins the game!</p>
+          <p className="score-count">Your Score: {score} <span className="icon icon-beer" /></p>
+        </section>
+        <section className={`game-board${gameActive ? ' game-active' : ''}`}>
+          <GithubKitty style={iconPosition}
+            className="github-icon"
+            onClick={this.handleSVGInteraction} />
+          {!gameActive && this.renderGameMessage()}
+        </section>
+      </div>
+    );
+  }
 }
 
-
-ReactDOM.render(<HomeSPA />, document.getElementById('react-spa'))
+ReactDOM.render(<HomeSPA />, document.querySelector('.react-app'))
